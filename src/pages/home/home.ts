331f95562f65a7c3409c521b5bb6e0eff2973249
Diego Nobre <dcnobre@gmail.com>
2017-12-23 08:12:40 -0300

    Initializing TurnJS

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+
+declare var $:any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform
+  ) {}
 
+  ionViewDidLoad() {
+    $("#flipbook").turn({
+      width: '100%',
+      height: this.platform.height() - 50,
+      display: 'single',
+      autoCenter: true
+    });
   }
-
 }
